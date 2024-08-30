fd45e4128231ced77239c4bba5ddec312b8a1662
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-08-30 14:54:22 +0100

    pep compliant -task 5

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -7,23 +7,27 @@ from flask_babel import gettext as _
 
 class Config():
     """config class"""
-    LANGUAGES = ["en","fr"]
+    LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app = Flask(__name__)
-app.config.from_object(Config) # add config class attr to the app.config instance
+#  add config class attr to the app.config instance
+app.config.from_object(Config)
 babel = Babel(app)
+
 
 @babel.localeselector
 def get_locale():
     """gets the langueage
     current user """
     if 'locale' in request.args:
-       locale = request.args.get('locale')
-       if locale in app.config['LANGUAGES']:
-           return locale
+        locale = request.args.get('locale')
+        if locale in app.config['LANGUAGES']:
+            return locale
     return app.config['BABEL_DEFAULT_LOCALE']
+
 
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
@@ -32,24 +36,30 @@ users = {
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
+
 def get_user(user_id):
     """get user from
     mocked database """
     return users.get(user_id)
-    
+
+
 @app.before_request
 def before_request():
+    """ before request """
     g.user = None
     login_id = request.args.get('login_as')
     if login_id:
         g.user = get_user(int(login_id))
 
+
 @app.route('/')
 def hello_world():
+    """show hello world to user"""
     username = ""
     if g.user:
         username = g.user.get('name')
     return render_template('5-index.html', username=username)
+
 
 if __name__ == "__main__":
     app.run()
