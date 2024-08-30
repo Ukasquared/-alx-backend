91b9df74258be0882f61adeea32ae240bc698bde
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-08-30 13:08:28 +0100

    task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    """a basic app"""
     return render_template('0-index.html')
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     app.run()
