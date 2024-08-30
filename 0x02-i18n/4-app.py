5a6b35fefad9add0e44bfa7662854ac1cf7dce43
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-08-30 13:41:27 +0100

    make pep compliant

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -7,17 +7,20 @@ from flask_babel import gettext as _
 
 class Config():
     """config class"""
-    LANGUAGES = ["en","fr"]
+    LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app = Flask(__name__)
-app.config.from_object(Config) # add config class attr to the app.config instance
+# add config class attr to the app config instance
+app.config.from_object(Config)
 babel = Babel(app)
+
 
 @babel.localeselector
 def get_locale():
-    """gets the langueage
+    """gets the language
     current user """
     if 'locale' in request.args:
        locale = request.args.get('locale')
@@ -25,11 +28,14 @@ def get_locale():
            return locale
     return app.config['BABEL_DEFAULT_LOCALE']
 
+
 @app.route('/')
 def hello_word():
+    """ show hello to the world"""
     home_title = "Welcome to Holberton"
     home_header = "Hello world!"
     return render_template('3-index.html')
+
 
 if __name__ == "__main__":
     app.run()
